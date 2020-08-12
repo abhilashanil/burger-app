aff149532ff3453bde30ae71a8b4f204b824ac38
Anjali Vattaparambath <avattapa@rmplc.net>
2020-08-12 15:49:10 +0530

    previewcss&index.php

--- burger-app-yii/frontend/views/burger/index.php
+++ burger-app-yii/frontend/views/burger/index.php
@@ -1,1 +1,25 @@
 <?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="Burger">
+        <div class="Burger_Breadtop">
+            <div class="Burger_seed1">
+            </div>
+            <div class="Burger_seed2">
+            </div>
+        </div>
+        <p>Please start adding ingredients!!!</p>
+        <div class="Burger_Breadbottom">
+        </div>
+    </div>
+</body>
+</html>
