ae87ca257186bb61aac5e864042ee56cce49433a
Abhilash A <abi.a.cool@gmail.com>
2020-08-12 14:58:19 +0530

    added burger controller

---
+++ burger-app-yii/frontend/controllers/BurgerController.php
@@ -0,0 +1,17 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * Burger controller
+ */
+class BurgerController extends Controller
+{
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
