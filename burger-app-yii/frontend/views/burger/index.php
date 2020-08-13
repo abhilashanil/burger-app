9f88ccf82652a13b735102ad04d4e7cd0c6119fb
Anjali Vattaparambath <avattapa@rmplc.net>
2020-08-13 14:03:13 +0530

    created preview.php

--- burger-app-yii/frontend/views/burger/index.php
+++ burger-app-yii/frontend/views/burger/index.php
@@ -1,25 +1,4 @@
 <?php
-
-
+    echo $this->render('preview');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div class="Burger">
-        <div class="Burger_Breadtop">
-            <div class="Burger_seed1">
-            </div>
-            <div class="Burger_seed2">
-            </div>
-        </div>
-        <p>Please start adding ingredients!!!</p>
-        <div class="Burger_Breadbottom">
-        </div>
-    </div>
-</body>
-</html>
+
