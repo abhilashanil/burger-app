806227ff3e2be6fb3525c29e26104fa6a48b7179
Revathy Jayanthi <rejayant@rmplc.net>
2020-08-14 14:34:40 +0530

    Add ingredient layout

---
+++ burger-app-yii/frontend/views/burger/CustomerOrderIngredients.php
@@ -0,0 +1,28 @@
+<?php
+
+
+?>
+<div class="BuildControl">
+   <p>Current Price : <strong>4.00</strong></p>
+   <div class="Build">
+      <div class="Build_Label">Salad</div>
+      <button class="Build_Less" disabled="">Less</button>
+      <button class="Build_More">More</button>
+    </div>
+    <div class="Build">
+      <div class="Build_Label">Cheese</div>
+      <button class="Build_Less" disabled="">Less</button>
+      <button class="Build_More">More</button>
+    </div>
+    <div class="Build">
+      <div class="Build_Label">Bacon</div>
+      <button class="Build_Less" disabled="">Less</button>
+      <button class="Build_More">More</button>
+    </div>
+    <div class="Build">
+      <div class="Build_Label">Meat</div>
+      <button class="Build_Less" disabled="">Less</button>
+      <button class="Build_More">More</button>
+    </div>
+    <button class="BuildControl_OrderButton" disabled=""> SIGN UP FOR ORDER</button>
+</div>
